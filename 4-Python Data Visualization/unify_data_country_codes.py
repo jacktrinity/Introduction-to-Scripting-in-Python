e6bde099b830489287923513fe42cbeb8908ba98
Jack Trinh <46969960+jacktrinity@users.noreply.github.com>
2021-02-24 18:15:28 -0800

    Create unify_data_country_codes.py

---
+++ 4-Python Data Visualization/unify_data_country_codes.py
@@ -0,0 +1,182 @@
+"""
+Project for Week 4 of "Python Data Visualization".
+Unify data via common country codes.
+
+CodeSkulptor: https://py3.codeskulptor.org/#save3_0Uvbyo1YwA.py
+"""
+
+import csv
+import math
+import pygal
+
+
+def read_csv_as_nested_dict(filename, keyfield, separator, quote):
+    """
+    Inputs:
+      filename  - Name of CSV file
+      keyfield  - Field to use as key for rows
+      separator - Character that separates fields
+      quote     - Character used to optionally quote fields
+
+    Output:
+      Returns a dictionary of dictionaries where the outer dictionary
+      maps the value in the key_field to the corresponding row in the
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
+    """
+    table = {}
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=separator, quotechar=quote)
+        for row in csvreader:
+            rowid = row[keyfield]
+            table[rowid] = row
+    return table
+
+
+def build_country_code_converter(codeinfo):
+    """
+    Inputs:
+      codeinfo      - A country code information dictionary
+
+    Output:
+      A dictionary whose keys are plot country codes and values
+      are world bank country codes, where the code fields in the
+      code file are specified in codeinfo.
+    """
+
+    filename = codeinfo["codefile"]
+    separator = codeinfo["separator"]
+    quote = codeinfo["quote"]
+
+    keyfield = codeinfo["plot_codes"]
+    data_code = codeinfo["data_codes"]
+
+    country_code_table = read_csv_as_nested_dict(filename, keyfield, separator, quote)
+
+    converted_country_code = {}
+    for keys, values in country_code_table.items():
+        converted_country_code[keys] = values[data_code]
+
+    return converted_country_code
+
+
+def reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries):
+    """
+    Inputs:
+      codeinfo       - A country code information dictionary
+      plot_countries - Dictionary whose keys are plot library country codes
+                       and values are the corresponding country name
+      gdp_countries  - Dictionary whose keys are country codes used in GDP data
+
+    Output:
+      A tuple containing a dictionary and a set.  The dictionary maps
+      country codes from plot_countries to country codes from
+      gdp_countries.  The set contains the country codes from
+      plot_countries that did not have a country with a corresponding
+      code in gdp_countries.
+
+      Note that all codes should be compared in a case-insensitive
+      way.  However, the returned dictionary and set should include
+      the codes with the exact same case as they have in
+      plot_countries and gdp_countries.
+    """
+    country_code = build_country_code_converter(codeinfo)
+
+    lower_case_code = {}
+    for keys, values in country_code.items():
+        lower_case_code[keys.lower()] = values.lower()
+
+    matched_name = {}
+    for two_letter in plot_countries:
+        for three_letter in gdp_countries:
+
+            if lower_case_code[two_letter.lower()] == three_letter.lower():
+                matched_name[two_letter] = three_letter
+
+    not_found = []
+    for missing_name in plot_countries:
+        if missing_name not in matched_name:
+            not_found.append(missing_name)
+
+    return matched_name, set(not_found)
+
+
+def build_map_dict_by_code(gdpinfo, codeinfo, plot_countries, year):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      codeinfo       - A country code information dictionary
+      plot_countries - Dictionary mapping plot library country codes to country names
+      year           - String year for which to create GDP mapping
+
+    Output:
+      A tuple containing a dictionary and two sets.  The dictionary
+      maps country codes from plot_countries to the log (base 10) of
+      the GDP value for that country in the specified year.  The first
+      set contains the country codes from plot_countries that were not
+      found in the GDP data file.  The second set contains the country
+      codes from plot_countries that were found in the GDP data file, but
+      have no GDP data for the specified year.
+    """
+
+    return
+
+
+def render_world_map(gdpinfo, codeinfo, plot_countries, year, map_file):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      codeinfo       - A country code information dictionary
+      plot_countries - Dictionary mapping plot library country codes to country names
+      year           - String year of data
+      map_file       - String that is the output map file name
+
+    Output:
+      Returns None.
+
+    Action:
+      Creates a world map plot of the GDP data in gdp_mapping and outputs
+      it to a file named by svg_filename.
+    """
+    return
+
+
+def test_render_world_map():
+    """
+    Test the project code for several years
+    """
+    gdpinfo = {
+        "gdpfile": "isp_gdp.csv",
+        "separator": ",",
+        "quote": '"',
+        "min_year": 1960,
+        "max_year": 2015,
+        "country_name": "Country Name",
+        "country_code": "Country Code"
+    }
+
+    codeinfo = {
+        "codefile": "isp_country_codes.csv",
+        "separator": ",",
+        "quote": '"',
+        "plot_codes": "ISO3166-1-Alpha-2",
+        "data_codes": "ISO3166-1-Alpha-3"
+    }
+
+    # Get pygal country code map
+    pygal_countries = pygal.maps.world.COUNTRIES
+
+    # 1960
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "1960", "isp_gdp_world_code_1960.svg")
+
+    # 1980
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "1980", "isp_gdp_world_code_1980.svg")
+
+    # 2000
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "2000", "isp_gdp_world_code_2000.svg")
+
+    # 2010
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "2010", "isp_gdp_world_code_2010.svg")
+
+
+# test_render_world_map()
